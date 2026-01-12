62511ffc9cec5618e8af406081684b86e2c1d204
Nayara <nayaretasan@gmail.com>
2026-01-12 19:37:31 +0100

    Modelos categoria y valoracion creados

--- fase_2/repo/custom_addons/loop_proyecto/models/categoria.py
+++ fase_2/repo/custom_addons/loop_proyecto/models/categoria.py
@@ -6,4 +6,24 @@ class categoria(models.Model):
     _name='categoria'
     _description='Modelo de categoría'
 
-    nombre = fields.Char()
+    nombre = fields.Char(
+        string='Nombre de la categoría',
+        required=True,
+        index=True,
+        store=True,
+        help='Nombre descriptivo de la categoría'
+    )
+
+    descripcion = fields.Text(
+        string='Descripción',
+        required=True,
+        store=True,
+        help='Descripción detallada de la categoría'
+    )
+
+    imagen = fields.Binary(
+        string='Imagen de la categoría',
+        required=True,
+        store=True,
+        help='Imagen representativa de la categoría'
+    )
---
+++ fase_2/repo/custom_addons/loop_proyecto/models/valoracion.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Valoracion(models.Model):
+
+    _name = 'loop_proyecto.valoracion'
+    _description = 'Valoracion'
+
+    comentario = fields.Char(
+        string='Comentario',
+        required=True,
+        size=1000,
+        store=True
+    )
+    
+    usuario_comentador = fields.Many2one(
+        comodel_name='res.users',
+        string='Usuario',
+        required=True,
+        store=True
+    )
+
+    usuario_valorado = fields.Many2one(
+        comodel_name='res.users',
+        string='Usuario',
+        required=True,
+    )
+
+    valoracion = fields.Selection(
+        string='Valoracion',
+        required=True,
+        help='Valoracion de 1 a 5 estrellas.'
+    )
