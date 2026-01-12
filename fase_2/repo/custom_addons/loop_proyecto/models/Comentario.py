a8ca3ad88df2097bd705e0fc5d05fa554bda0f80
Fabiana <fabhunalv@alu.edu.gva.es>
2026-01-12 20:04:32 +0100

    Modelo comentario creado

---
+++ fase_2/repo/custom_addons/loop_proyecto/models/Comentario.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class Comentario(models.Model):
+    _name = 'loop_proyecto.comentario'
+    _description = 'Comentario de productos'
+
+    contenido = fields.Text(string='Contenido del comentario', required=True)
+    fecha_creacion = fields.Datetime(string='Fecha de creación', default=fields.Datetime.now, readonly=True)
+    
+    comentador_id = fields.Many2one(
+        'res.users',
+        string='Usuario autor',
+        default=lambda self: self.env.user,
+        readonly=True
+    )
+    
+    producto_id = fields.Many2one(
+        'marketplace.producto',
+        string='Producto',
+        required=True,
+        index=True,
+        ondelete='cascade'
+    )
+    
+    estado = fields.Selection(
+        [('published', 'Publicado'), ('hidden', 'Oculto'), ('deleted', 'Eliminado')],
+        string='Estado',
+        default='published',
+        index=True
+    )
+    
+    moderador_id = fields.Many2one('res.users', string='Moderado por')
+    fecha_moderacion = fields.Datetime(string='Fecha moderación')
