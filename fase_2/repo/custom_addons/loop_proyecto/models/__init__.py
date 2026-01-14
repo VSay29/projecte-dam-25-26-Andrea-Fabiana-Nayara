6f3bcb71499545665746bdd128ea7bc15dfd479d
Nayara <nayaretasan@gmail.com>
2026-01-14 20:16:36 +0100

    Correción de errores

--- fase_2/repo/custom_addons/loop_proyecto/models/__init__.py
+++ fase_2/repo/custom_addons/loop_proyecto/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import categoria
-from . import Producto
-from . import Comentario
-from . import Denuncia
+from . import producto
+from . import comentario
+from . import denuncia
 from . import etiqueta
 from . import imagen_producto
 from . import valoracion
