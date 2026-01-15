6b4dc75b0cec571e21e9067efcc07ba578867472
Fabiana <fabhunalv@alu.edu.gva.es>
2026-01-15 18:32:15 +0100

    Vista categoria creada

--- fase_2/repo/custom_addons/loop_proyecto/__manifest__.py
+++ fase_2/repo/custom_addons/loop_proyecto/__manifest__.py
@@ -31,9 +31,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/vista_categoria.xml',
-        'views/vista_producto.xml',
-        'views/vista_denuncia.xml',
-        'views/vista_usuario.xml',
+       # 'views/vista_producto.xml',
+       # 'views/vista_denuncia.xml',
+       # 'views/vista_usuario.xml',
     ],
 }
 
