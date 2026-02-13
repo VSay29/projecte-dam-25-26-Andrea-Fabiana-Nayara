b0a43c07de106bd0569fae6b06fa3c4a3314ec1f
Fabiana <fabhunalv@alu.edu.gva.es>
2026-02-13 17:43:42 +0100

    Actualizaciones pantalla de ajustes

--- fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/SettingsActivity.kt
+++ fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/SettingsActivity.kt
@@ -2,16 +2,23 @@ package com.example.android_loop
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android_loop.ui.theme.Android_LoopTheme
@@ -34,7 +41,42 @@ class SettingsActivity {
                 verticalArrangement = Arrangement.Top
             ) {
                 Spacer(modifier = Modifier.height(30.dp))
-                Text("Pantalla de ajustes")
+                Text("Pantalla de ajustes",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center)
+                OptionsLazyColumn()
+            }
+        }
+    }
+
+    @Composable
+    fun OptionsLazyColumn() {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            items(5) { index ->
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+
+                        Text("Opción ${index + 1}")
+
+                    }
+                }
             }
         }
     }
