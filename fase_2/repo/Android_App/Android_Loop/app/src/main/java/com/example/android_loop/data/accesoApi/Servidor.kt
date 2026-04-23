ac35d321ebec21fde02f27e51094ebba5e030143
Nay <162121688+VSay29@users.noreply.github.com>
2026-04-23 10:46:01 +0200

    Delete fase_2/repo/Android_App/Android_Loop directory
    Borrar proyecto viejo

--- fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/data/accesoApi/Servidor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android_loop.data.accesoApi
-
-object Servidor {
-    //val BASE_URL = "http://localhost:8069"  // Solo emulador
-    //val BASE_URL = "http://10.0.2.2:8069"  // Emulador alternativo
-    val BASE_URL = "http://10.26.32.229:8069" // IP actual
-}
