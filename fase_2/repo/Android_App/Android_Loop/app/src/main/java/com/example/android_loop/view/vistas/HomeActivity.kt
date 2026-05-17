62e42b2727700b54c7f2d5845a1146dec4e370fc
Nay <162121688+VSay29@users.noreply.github.com>
2026-05-17 18:24:30 +0200

    Update HomeActivity.kt

--- fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/view/vistas/HomeActivity.kt
+++ fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/view/vistas/HomeActivity.kt
@@ -196,30 +196,11 @@ fun Home(navController: NavHostController) {
             // SECCION: Barra de búsqueda
             Spacer(modifier = Modifier.height(12.dp))
 
-            TextField(
+            Busqueda_Componente(
                 value = buscador,
                 onValueChange = { buscador = it },
-                placeholder = { Text("Buscar productos...") },
-                leadingIcon = {
-                    Icon(
-                        painter = painterResource(R.drawable.lupa),
-                        contentDescription = "Buscar",
-                        modifier = Modifier.size(20.dp)
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .clip(RoundedCornerShape(30.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFFF0F4F8),
-                    unfocusedContainerColor = Color(0xFFF0F4F8),
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent
-                )
-            )
+                placeholder = "Buscar productos...",
+                modifier = Modifier.padding(horizontal = 16.dp)
 
             Spacer(modifier = Modifier.height(12.dp))
 
